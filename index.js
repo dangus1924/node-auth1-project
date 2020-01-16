41aeb5343dd15218aaeb5e4ee225329f0c9df611
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2020-01-15 19:42:44 -0500

    created knexfile

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const server = express()
 const port = process.env.PORT || 5000
 
 server.get('/', (req, res, next) => {
-    res.send('<h1>Hello World</h1>')
+    res.send('<h1>User Database</h1>')
 })
 
 server.listen(port, () => console.log(`Server is listening at port: ${port}`))
