a1a1cad58124db059e1183d312b42784e84d719d
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2020-01-15 20:26:25 -0500

    created user model and router

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const helmet = require('helmet')
 const server = express()
+const cors = require('cors')
+
 const port = process.env.PORT || 5000
+
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
 
 server.get('/', (req, res, next) => {
     res.send('<h1>User Database</h1>')
+})
+
+server.use((err, req, res, next) => {
+    console.log('Error:', err)
+
+    res.status(500).json({
+        message: 'Uh-Oh something is broken!'
+    })
 })
 
 server.listen(port, () => console.log(`Server is listening at port: ${port}`))
