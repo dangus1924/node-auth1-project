99fa0649907c91b4d5491715b9f1ba7eb9b2128a
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2020-01-16 19:13:13 -0500

    fixed spelling error

---
+++ auth/auth-router.js
@@ -0,0 +1,38 @@
+const bcrypt = require('bcryptjs')
+const express = require('express')
+const userModel = require('../users/users-model')
+
+const router = express.Router()
+
+router.post('/register', async (req, res, next) => {
+    try {
+        const saved = await userModel.add(req.body)
+        res.status(201).json(saved)
+
+    } catch (err){
+        next(err)
+    }
+})
+
+router.post('/login', async (req, res, next) => {
+    try {
+        const {username, password } = req.body
+        const user = await userModel.findBy({ username }).first()
+        const passwordValid = await bcrypt.compare(password, user.password)
+
+        if(user && passwordValid) {
+            res.status(200).json({
+                message: `Welcome ${user.username}!`
+            })
+        } else {
+            res.status(401).json({
+                message: 'Username && || password incorrect'
+            })
+        }
+
+    } catch(err) {
+        next(err)
+    }
+})
+
+module.exports = router
