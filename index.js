9f7052ad27e4c135679404e6366c005e4e0ec682
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2020-01-20 19:57:20 -0500

    added both knex and express session into index

--- index.js
+++ index.js
@@ -2,22 +2,39 @@ const express = require('express');
 const helmet = require('helmet')
 const server = express()
 const cors = require('cors')
+const session = require('express-session')
+const knexSessionsStore = require('connect-session-knex')
+
+const dbconfig = require('./data/db-config')
 const authRouter = require('./auth/auth-router')
 const userRouter = require('./users/users-router')
-const session = require('express-session')
+
 
 const port = process.env.PORT || 5000
 
 server.use(helmet())
 server.use(cors())
 server.use(express.json())
+server.use(session({
+    name:'unknown',
+    resave: false,
+    saveUninitialized: false,
+    secret: 'this is the secret key, no one knows how about you!',
+    cookies: {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        secure: false,
+    },
+    store: new knexSessionsStore({
+        knex: dbconfig,
+        createtable: true,
+    })    
+}))
 
 server.use('/auth', authRouter)
 server.use('/users', userRouter)
 
-// server.get('/', (req, res, next) => {
-//     res.send('<h1>User Database</h1>')
-// })
+
 
 server.use((err, req, res, next) => {
     console.log('Error:', err)
